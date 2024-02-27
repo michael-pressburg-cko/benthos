2f56b68b6efb462c0b3ae206f0379902f556ce67
Michael Pressburg <michael.pressburg@checkout.com>
2024-02-27 09:53:43 +0000

    comment2

--- internal/impl/aws/input_kinesis_enhanced_fanout.go
+++ internal/impl/aws/input_kinesis_enhanced_fanout.go
@@ -1,10 +1,218 @@
 package aws
 
-import "sync"
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+	"github.com/cenkalti/backoff/v4"
+)
 
 func (k *kinesisReader) runEfoConsumer(wg *sync.WaitGroup, info streamInfo, shardID, startingSequence string) (initErr error) {
 	//register consumer
 	// busy read from consumer
 	// copy code from runConsumer and use a channel to pass to pending
+	defer func() {
+		if initErr != nil {
+			wg.Done()
+			if _, err := k.checkpointer.Checkpoint(context.Background(), info.id, shardID, startingSequence, true); err != nil {
+				k.log.Errorf("Failed to gracefully yield checkpoint: %v\n", err)
+			}
+		}
+	}()
+
+	// Stores records, batches them up, and provides the batches for dispatch,
+	// whilst ensuring only N records are in flight at a given time.
+	var recordBatcher *awsKinesisRecordBatcher
+	if recordBatcher, initErr = k.newAWSKinesisRecordBatcher(info, shardID, startingSequence); initErr != nil {
+		return initErr
+	}
+
+	// Keeps track of retry attempts.
+	boff := k.boffPool.Get().(backoff.BackOff)
+
+	// Stores consumed records that have yet to be added to the batcher.
+	var pending []types.Record
+	var iter string
+	if iter, initErr = k.getIter(info, shardID, startingSequence); initErr != nil {
+		return initErr
+	}
+
+	// Keeps track of the latest state of the consumer.
+	state := awsKinesisConsumerConsuming
+	var pendingMsg asyncMessage
+
+	unblockedChan, blockedChan := make(chan time.Time), make(chan time.Time)
+	close(unblockedChan)
+
+	// Channels (and contexts) representing the four main actions of the
+	// consumer goroutine:
+	// 1. Timed batches, this might be nil when timed batches are disabled.
+	// 2. Record pulling, this might be unblocked (closed channel) when we run
+	//    out of pending records, or a timed channel when our last attempt
+	//    yielded zero records.
+	// 3. Message flush, this is the target of our current batched message, and
+	//    is nil when our current batched message is a zero value (we don't have
+	//    one prepared).
+	// 4. Next commit, is "done" when the next commit is due.
+	var nextTimedBatchChan <-chan time.Time
+	var nextPullChan <-chan time.Time = unblockedChan
+	var nextFlushChan chan<- asyncMessage
+	commitCtx, commitCtxClose := context.WithTimeout(k.ctx, k.commitPeriod)
+
+	go func() {
+		defer func() {
+			commitCtxClose()
+			recordBatcher.Close(context.Background(), state == awsKinesisConsumerFinished)
+			boff.Reset()
+			k.boffPool.Put(boff)
+
+			reason := ""
+			switch state {
+			case awsKinesisConsumerFinished:
+				reason = " because the shard is closed"
+				if err := k.checkpointer.Delete(k.ctx, info.id, shardID); err != nil {
+					k.log.Errorf("Failed to remove checkpoint for finished stream '%v' shard '%v': %v", info.id, shardID, err)
+				}
+			case awsKinesisConsumerYielding:
+				reason = " because the shard has been claimed by another client"
+				if err := k.checkpointer.Yield(k.ctx, info.id, shardID, recordBatcher.GetSequence()); err != nil {
+					k.log.Errorf("Failed to yield checkpoint for stolen stream '%v' shard '%v': %v", info.id, shardID, err)
+				}
+			case awsKinesisConsumerClosing:
+				reason = " because the pipeline is shutting down"
+				if _, err := k.checkpointer.Checkpoint(context.Background(), info.id, shardID, recordBatcher.GetSequence(), true); err != nil {
+					k.log.Errorf("Failed to store final checkpoint for stream '%v' shard '%v': %v", info.id, shardID, err)
+				}
+			}
+
+			wg.Done()
+			k.log.Debugf("Closing stream '%v' shard '%v' as client '%v'%v", info.id, shardID, k.checkpointer.clientID, reason)
+		}()
+
+		k.log.Debugf("Consuming stream '%v' shard '%v' as client '%v'", info.id, shardID, k.checkpointer.clientID)
+
+		// Switches our pull chan to unblocked only if it's currently blocked,
+		// as otherwise it's set to a timed channel that we do not want to
+		// disturb.
+		unblockPullChan := func() {
+			if nextPullChan == blockedChan {
+				nextPullChan = unblockedChan
+			}
+		}
+
+		for {
+			var err error
+			// TODO CHANGE HERE
+			if state == awsKinesisConsumerConsuming && len(pending) == 0 && nextPullChan == unblockedChan {
+				if pending, iter, err = k.getRecords(info, shardID, iter); err != nil {
+					if !awsErrIsTimeout(err) {
+						nextPullChan = time.After(boff.NextBackOff())
+
+						var aerr *types.ExpiredIteratorException
+						if errors.As(err, &aerr) {
+							k.log.Warn("Shard iterator expired, attempting to refresh")
+							newIter, err := k.getIter(info, shardID, recordBatcher.GetSequence())
+							if err != nil {
+								k.log.Errorf("Failed to refresh shard iterator: %v", err)
+							} else {
+								iter = newIter
+							}
+						} else {
+							k.log.Errorf("Failed to pull Kinesis records: %v\n", err)
+						}
+					}
+				} else if len(pending) == 0 {
+					nextPullChan = time.After(boff.NextBackOff())
+				} else {
+					boff.Reset()
+					nextPullChan = blockedChan
+				}
+				// The getRecords method ensures that it returns the input
+				// iterator whenever it errors out. Therefore, regardless of the
+				// outcome of the call if iter is now empty we have definitely
+				// reached the end of the shard.
+				if iter == "" {
+					state = awsKinesisConsumerFinished
+				}
+			} else {
+				unblockPullChan()
+			}
+
+			if pendingMsg.msg == nil {
+				// If our consumer is finished and we've run out of pending
+				// records then we're done.
+				if len(pending) == 0 && state == awsKinesisConsumerFinished {
+					if pendingMsg, _ = recordBatcher.FlushMessage(k.ctx); pendingMsg.msg == nil {
+						return
+					}
+				} else if recordBatcher.HasPendingMessage() {
+					if pendingMsg, err = recordBatcher.FlushMessage(commitCtx); err != nil {
+						k.log.Errorf("Failed to dispatch message due to checkpoint error: %v\n", err)
+					}
+				} else if len(pending) > 0 {
+					var i int
+					var r types.Record
+					for i, r = range pending {
+						if recordBatcher.AddRecord(r) {
+							if pendingMsg, err = recordBatcher.FlushMessage(commitCtx); err != nil {
+								k.log.Errorf("Failed to dispatch message due to checkpoint error: %v\n", err)
+							}
+							break
+						}
+					}
+					if pending = pending[i+1:]; len(pending) == 0 {
+						unblockPullChan()
+					}
+				} else {
+					unblockPullChan()
+				}
+			}
+
+			if pendingMsg.msg != nil {
+				nextFlushChan = k.msgChan
+			} else {
+				nextFlushChan = nil
+			}
+
+			if nextTimedBatchChan == nil {
+				if tNext, exists := recordBatcher.UntilNext(); exists {
+					nextTimedBatchChan = time.After(tNext)
+				}
+			}
+
+			select {
+			case <-commitCtx.Done():
+				if k.ctx.Err() != nil {
+					// It could've been our parent context that closed, in which
+					// case we exit.
+					state = awsKinesisConsumerClosing
+					return
+				}
+
+				commitCtxClose()
+				commitCtx, commitCtxClose = context.WithTimeout(k.ctx, k.commitPeriod)
+
+				stillOwned, err := k.checkpointer.Checkpoint(k.ctx, info.id, shardID, recordBatcher.GetSequence(), false)
+				if err != nil {
+					k.log.Errorf("Failed to store checkpoint for Kinesis stream '%v' shard '%v': %v", info.id, shardID, err)
+				} else if !stillOwned {
+					state = awsKinesisConsumerYielding
+					return
+				}
+			case <-nextTimedBatchChan:
+				nextTimedBatchChan = nil
+			case nextFlushChan <- pendingMsg:
+				pendingMsg = asyncMessage{}
+			case <-nextPullChan:
+				nextPullChan = unblockedChan
+			case <-k.ctx.Done():
+				state = awsKinesisConsumerClosing
+				return
+			}
+		}
+	}()
 	return nil
 }
